993885afbb99b55ca218b01b58edbc4eeb74ea62
Piraty <piraty1@inbox.ru>
2022-08-10 12:41:57 +0200

    tests: time invocations

--- tests/round-robin.sh
+++ tests/round-robin.sh
@@ -12,7 +12,8 @@ error() {
 PBR_INDEX_FILE="$(mktemp)"
 export PBR_INDEX_FILE
 
-for i in $(seq 10); do
+for _ in $(seq 10); do
 	sleep 1 # be nice
-	date +%s | ./pbr
+	time -p sh -c 'date +%s | ./pbr'
+	echo
 done
